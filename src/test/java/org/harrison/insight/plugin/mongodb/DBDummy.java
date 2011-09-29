22b0cbf342feb82f20d70e5901a88ffe8a73375e
jkew <jkew@vmware.com>
2011-09-29 16:51:49 -0700

    first of six unit tests

---
+++ src/test/java/org/harrison/insight/plugin/mongodb/DBDummy.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2009-2010 VMware.
+ * All rights reserved.
+ */
+
+package org.harrison.insight.plugin.mongodb;
+
+import com.mongodb.CommandResult;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+
+/**
+ */
+public class DBDummy extends DB {
+
+    public DBDummy(Mongo mongo, String name) {
+        super(mongo, name);
+    }
+
+    public CommandResult command(com.mongodb.DBObject cmd)
+            throws com.mongodb.MongoException {
+        return null;
+    }
+
+    public CommandResult command(com.mongodb.DBObject cmd, int options)
+            throws com.mongodb.MongoException {
+        return null;
+    }
+
+    public CommandResult command(java.lang.String cmd)
+            throws com.mongodb.MongoException {
+        return null;
+    }
+
+
+    @Override
+    public void requestStart() {}
+
+    @Override
+    public void requestDone() {}
+
+    @Override
+    public void requestEnsureConnection() {}
+
+    @Override
+    protected DBCollection doGetCollection(String s) {
+        return null;
+    }
+}
---
+++ src/test/java/org/harrison/insight/plugin/mongodb/MongoDbOperationCollectionAspectTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2009-2010 VMware.
+ * All rights reserved.
+ */
+
+package org.harrison.insight.plugin.mongodb;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.springsource.insight.collection.OperationCollectionAspectSupport;
+import com.springsource.insight.collection.OperationCollectionAspectTestSupport;
+import com.springsource.insight.intercept.operation.Operation;
+import com.springsource.insight.intercept.operation.OperationList;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ */
+public class MongoDbOperationCollectionAspectTest
+        extends OperationCollectionAspectTestSupport {
+
+    // execution(CommandResult DB.command(..));
+    @Test
+    public void dbCommand() throws Exception {
+        Mongo mongo = mock(Mongo.class);
+        //DB db = mock(DB.class);
+        DB db = new DBDummy(mongo, "my thing");
+        db.command("Hello there");
+        Operation op = (Operation) getLastEntered(Operation.class);
+        op.finalizeConstruction();
+        assertNotNull(op);
+        assertEquals("MongoDB: DB.command()", op.getLabel());
+        assertEquals(MongoDbOperationCollectionAspect.TYPE, op.getType());
+        assertEquals("Hello there", ((OperationList)op.get("args")).get(0));
+
+    }
+
+    @Override
+    public OperationCollectionAspectSupport getAspect() {
+        return MongoDbOperationCollectionAspect.aspectOf();
+    }
+}
